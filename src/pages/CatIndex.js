ccbd280990061b3d10f82272b066b7119a130b46
Aubzzz <aubrey.diehl28@gmail.com>
2023-06-09 16:35:34 -0700

    aubrey edited first two bullet points for read functionality

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,11 +1,40 @@
 import React from "react"
+import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap"
 
-const CatIndex = () => {
+const CatIndex = ({cats}) => {
   return(
-    <>
-      <h3>This is the cat index</h3>
-    </>
-)}
+    <main className="cat-index-cards">
+      {cats.map((cat, index) => {
+      return (
+        <>
+          <Card
+  style={{
+    width: '18rem'
+  }}
+    key={index}>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"/>
+  <CardBody>
+    <CardTitle tag="h5">
+      {cat.name}
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6">
+      age: {cat.age}
+    </CardSubtitle>
+    <Button>
+      Button
+    </Button>
+  </CardBody>
+</Card>
+        </>
+      )
+})}    
+    </main>
+)
+}
 
 
 
