02e1d96962600c4ef2041204f8947329b2c42e02
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-09 13:15:06 -0500

    good tests on all components

---
+++ src/__test__/Footer.test.js
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import Footer from "../components/Footer"
+
+describe("<Footer />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    render (
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+      div
+    )
+  })
+  it("renders a src and alt", () => {
+    const div = document.createElement("div")
+    render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+        div
+    )
+  })
+  it("renders with text", () => {
+    const div = document.createElement("div")
+    render (
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+      div
+    )
+    const footerText = screen.getByText(/cat tinder footer/i)
+    expect(footerText).toBeInTheDocument()
+  })
+})
+
+
+export default Footer
---
+++ src/__test__/Header.test.js
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import Header from "../components/Header"
+
+describe("<Header />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    render (
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+      div
+    )
+  })
+  it("renders a src and alt", () => {
+    const div = document.createElement("div")
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+        div
+    )
+  })
+  it("renders with text", () => {
+    const div = document.createElement("div")
+    render (
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+      div
+    )
+    const headerText = screen.getByText(/cat tinder/i)
+    expect(headerText).toBeInTheDocument()
+  })
+})
+
+
+export default Header
